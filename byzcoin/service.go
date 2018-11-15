a888122285c2c4c4723d19f744d098ab036e8640
Linus Gasser <linus.gasser@epfl.ch>
2018-11-15 16:36:41 +0100

    more improvements

--- byzcoin/service.go
+++ byzcoin/service.go
@@ -1927,6 +1927,9 @@ func (s *Service) startAllChains() error {
 		if err != nil {
 			panic("getLeader should not return an error if roster is initialised.")
 		}
+
+		sb := s.db().GetByID(gen)
+		log.Print(s.ServerIdentity(), "has leader", leader, "in block", sb.Roster)
 		if leader.Equal(s.ServerIdentity()) {
 			s.pollChanMut.Lock()
 			s.pollChanWG.Add(1)
